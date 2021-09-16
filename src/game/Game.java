1c4dec00b75fe61c1aca1e185877444951567f75
Angelina <angelania@ya.ru>
2021-09-16 19:52:23 +0300

    add first version

---
+++ src/game/Game.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Angelina
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        int num = random.nextInt(5-0+1)+0;
+        
+        System.out.println("Guess the number from 0 to 5: ");
+        Scanner scanner = new Scanner(System.in);
+        int userNum = scanner.nextInt();
+        
+        if (num == userNum) {
+            System.out.println("You've guessed it! Yay! :)");
+        }else{
+            System.out.println("Wrong number. You've lost. :( Guessed number: "+num);
+        }
+        
+    }
+    
+}
