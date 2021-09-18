48f7e53122dbedf27a3ba7dd2916740ba3b1c7d6
Angelina <angelania@ya.ru>
2021-09-18 23:35:09 +0300

    added the option to exit the game

--- src/game/Game.java
+++ src/game/Game.java
@@ -19,14 +19,18 @@ public class Game {
      */
     public static void main(String[] args) {
         Random random = new Random();
+        String repeat = "";
+                
+        do{
         int num = random.nextInt(5-0+1)+0;
         
         System.out.println("Guess the number from 0 to 5: ");
         Scanner scanner = new Scanner(System.in);
         int attempt = 0;
-        
+        int userNum;
+
         do{
-        int userNum = scanner.nextInt();
+        userNum = scanner.nextInt();
         if (num == userNum) {
             System.out.println("You've guessed it! Yay! :)");
             break;
@@ -34,12 +38,19 @@ public class Game {
             if(attempt<2) {
                 System.out.println("Wrong. try again.");
             }else{
-                System.out.println("Wrong number. You've lost. :( Guessed number: "+num);
+                System.out.println("Wrong number. You've lost. :( Guessed number: " + num);
             }
         }
-        
         attempt++;
         }while(attempt < 3);
-    }
-    
+        System.out.println("-----------------------------");
+        System.out.printf("Press \"n\" to end the game,%npress any other button to continue: ");
+        scanner.nextLine(); //Надо опустошить предыдущее внесённое число, иначе сам продолжит дальше
+        repeat = scanner.nextLine();
+        if(repeat.equals("n")) { //Лучше в фигурных скобках писать, чем в одну строчку
+            break;
+        }
+        System.out.println("");
+    }while (true);
+}
 }
